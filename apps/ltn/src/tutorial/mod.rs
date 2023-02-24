52120c7800d3a52ea28bbb49c0499fede2d33f25
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2023-02-24 23:08:57 +0100

    Planning the tutorial out using comments

--- apps/ltn/src/tutorial/mod.rs
+++ apps/ltn/src/tutorial/mod.rs
@@ -9,3 +9,16 @@ pub struct Tutorial {
 impl Tutorial {
     pub fn new_state() -> Box<dyn State<App>> {}
 }
+
+/*
+TUTORIAL P1:
+Screen 1: Centered -- BTN's
+Screen 2: Custom Pos -- Arrow Box Left -- Popup on side needs to be open -- Everything needs to be grayed out except for 1 part
+Screen 3: Change bottom panel -- Arrow Box Down -- Everything needs to be grayed out except for 1 part
+
+To achieve the grayed out except for 1 part I can render that part of the popup/panel again only ontop of the grayed part. This way I prevent having to work with masks.
+    Check how other tutorial for game achieved this effect.
+        It looks like it does not do that.
+
+Event checking. AKA make sure the user has done what it is supposed to do before continueing the tutorial (or next button has to be pressed)
+*/
